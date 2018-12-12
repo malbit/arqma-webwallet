6551bfa8f9c9e944c3c9bc7a8f6e94d72e969aae
Michal vel m@lbit <malbit@users.noreply.github.com>
2018-12-12 14:42:57 +0000

    native went off

--- www/workers/TransferProcessingEntrypoint.ts
+++ www/workers/TransferProcessingEntrypoint.ts
@@ -7,10 +7,14 @@ importScripts('../lib/require.js');
 importScripts('../lib/biginteger.js');
 importScripts('../config.js');
 importScripts('../lib/base58.js');
+importScripts('../model/Cn.js');
 importScripts('../lib/crypto.js');
 importScripts('../lib/nacl-fast.js');
 importScripts('../lib/nacl-util.min.js');
 importScripts('../lib/sha3.js');
+
+require(['./TransferProcessing.js'], function (App) {});
+/*
 
 try {
 	importScripts('../lib/cn_utils_native.js');
@@ -20,3 +24,4 @@ try {
 }catch(e){
 	//TODO send error
 }
+*/
