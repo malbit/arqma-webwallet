b567286a6a262f8fb73cee95e5a716810dc5a58c
Michal vel m@lbit <michal@arqma.com>
2018-12-29 06:51:19 +0000

    fix2

--- www/model/blockchain/BlockchainExplorerRpc2.ts
+++ www/model/blockchain/BlockchainExplorerRpc2.ts
@@ -393,7 +393,7 @@ export class BlockchainExplorerRpc2 implements BlockchainExplorer{
 						continue;
 					}
 
-				  let output_idx_in_tx = Math.floor(Math.random()*out.vout.length);
+				  let output_idx_in_tx = Math.floor(Math.random()*iOut.vout.length);
 				  let extras = TransactionsExplorer.parseExtra(tx.extra);
 					let publicKey = '';
 					for(let extra of extras)
