0c451bea6923ce590d5fffbb69dd4c5c882443ef
Michal vel m@lbit <michal@arqma.com>
2018-12-30 13:57:32 +0000

    .

--- www/model/Storage.ts
+++ www/model/Storage.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2018, Gnock
+ * Copyright (c) 2018, The Masari Project
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 interface StorageInterface {
 	setItem(key: string, value: string): Promise<void>;
 	getItem(key: string, defaultValue: any): Promise<any>;
@@ -42,6 +57,79 @@ class LocalStorage implements StorageInterface{
 	}
 }
 
+
+class NativeStorageWrap implements StorageInterface{
+	setItem(key: string, value: any): Promise<void> {
+		return new Promise<void>(function (resolve, reject) {
+			if(window.NativeStorage)
+				window.NativeStorage.setItem(key,value,function(){
+					resolve();
+				}, function(error : NativeNativeStorageError){
+					reject();
+				});
+			else
+				reject();
+		});
+	}
+
+	getItem(key: string, defaultValue: any = null): Promise<any> {
+		return new Promise<any>(function (resolve, reject) {
+			if(window.NativeStorage)
+				window.NativeStorage.getItem(key,function(){
+					resolve();
+				}, function(error : NativeNativeStorageError){
+					if(error.code === 2)
+						resolve(defaultValue);
+					reject();
+				});
+			else
+				reject();
+		});
+	}
+
+	keys(): Promise<string[]> {
+		return new Promise<string[]>(function (resolve, reject) {
+			if(window.NativeStorage)
+				window.NativeStorage.keys(function(keys : string[]){
+					resolve(keys);
+				}, function(error : NativeNativeStorageError){
+					reject();
+				});
+			else
+				reject();
+		});
+	}
+
+	remove(key: string): Promise<void> {
+		return new Promise<void>(function (resolve, reject) {
+			if(window.NativeStorage)
+				window.NativeStorage.remove(key,function(){
+					resolve();
+				}, function(error : NativeNativeStorageError){
+					if(error.code === 2 || error.code === 3 || error.code === 4)
+						resolve();
+					reject();
+				});
+			else
+				reject();
+		});
+	}
+
+	clear(): Promise<void> {
+		return new Promise<void>(function (resolve, reject) {
+			if(window.NativeStorage)
+				window.NativeStorage.clear(function(){
+					resolve();
+				}, function(error : NativeNativeStorageError){
+					reject();
+				});
+			else
+				reject();
+		});
+	}
+}
+
+
 export class Storage{
 	static _storage : StorageInterface = new LocalStorage();
 
@@ -69,4 +157,8 @@ export class Storage{
 		return Storage._storage.setItem(key,value);
 	}
 
+}
+
+if(window.NativeStorage){
+	Storage._storage = new NativeStorageWrap();
 }
